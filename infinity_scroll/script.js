06887d8c6ac46d01b7c29cdf91dbbedead0c9305
Kevin O'Malley <kevin.omalley@cleverbridge.com>
2020-07-28 16:17:11 -0500

    finishes infinity scroll project

--- infinity_scroll/script.js
+++ infinity_scroll/script.js
@@ -1,12 +1,20 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+let isInitialLoad = true;
 
 // Unsplash API
-const count = 10;
+let initialCount = 5;
 const apiKey = 'GbYJNsVYsl63-rzGUiQJkq_5Lruh-a2jbwe60U-8hWg';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${initialCount}`;
+
+function updateAPIURLWithNewCount(updatedCount) {
+    apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${updatedCount}`;
+}
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -15,8 +23,19 @@ function setAttributes(element, attributes) {
     }
 }
 
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
         setAttributes(item, {
@@ -29,6 +48,8 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description
         });
+        // Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
@@ -40,12 +61,24 @@ async function getPhotos() {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
         displayPhotos();
+        if (isInitialLoad) {
+            updateAPIURLWithNewCount(30);
+            isInitialLoad = false;
+        }
     } catch (error) {
         // Catch Error
+        console.log('Oops, error', error)
     }
 }
 
-
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+    const targetOffsetHeight = document.body.offsetHeight - 1000;
+    if (window.innerHeight + window.scrollY >= targetOffsetHeight) {
+        loader.hidden = false;
+        getPhotos();
+    }
+});
 
 
 // On Load
