18a9912b953814edda758be503f510dc1c8acb03
Kevin O'Malley <kevin.omalley@cleverbridge.com>
2020-08-06 14:57:21 -0500

    adds light and dark mode toggling to portfolio

---
+++ --template-light-dark-mode/script.js
@@ -0,0 +1,46 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+function imageMode(theme) {
+    image1.src = `img/undraw_hello_${theme}.svg`;
+    image2.src = `img/undraw_hello_${theme}.svg`;
+    image3.src = `img/undraw_hello_${theme}.svg`;
+}
+
+// Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
+
+// Light Mode Styles
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
+// Switch Theme
+function switchTheme(event) {
+    // Enable dark mode if toggle is checked
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
+    }
+};
+
+// Event Listeners
+toggleSwitch.addEventListener('change', switchTheme);
