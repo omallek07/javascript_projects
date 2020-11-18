0200f6a56e2e9246efba7230a554ea0c7e1ec691
Kevin O'Malley <kevin.omalley@cleverbridge.com>
2020-11-18 08:50:10 -0600

    completes book keeper project

--- book-keeper/script.js
+++ book-keeper/script.js
@@ -35,6 +35,43 @@ function validate(nameValue, urlValue) {
     return true;
 }
 
+// Build Bookmarks DOM
+function buildBookmarks(){
+    // Remove all bookmark elements
+    bookmarksContainer.textContent = '';
+    // Build items
+    bookmarks.forEach(function(bookmark) {
+        const {
+            name,
+            url
+        } = bookmark;
+        // Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // Close Icon
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fa', 'fa-times');
+        closeIcon.setAttribute('title', 'Delete Bookmark');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+        // Favicon / Link Container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+        // Favicon
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alit', 'Favicon');
+        // Link
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+        // Append to bookmarks container
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.appendChild(item);
+    });
+}
+
 // Fetch Bookmarks
 function fetchBookmarks() {
     // Get bookmarks from LocalStorage if available
@@ -50,6 +87,26 @@ function fetchBookmarks() {
         ];
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
+    buildBookmarks();
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+
+    /* If bookmarks was an object structure
+        if (bookmarks[id]) {
+            delete bookmarks[id]
+        }
+    */
+
+    bookmarks.forEach((bookmark, idx) => {
+        if (bookmark.url === url) {
+            bookmarks.splice(idx, 1);
+        }
+    });
+    // Update bookmarks array in localStorage, re-populate DOM
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
 }
 
 // Handle data from Form
